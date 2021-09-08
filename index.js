e26df6cea27035402178cef897c639f77bdc8113
Tselate <Tselatedawit@outlook.com>
2021-09-07 23:44:01 -0600

    Almsot done

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ registartionForm.addEventListener("submit", event => {
     let partnerFullName = registrationData.get("fullname")
 
     let newHtmlContent = `
-        <h2> Congradulation, ${partnerFullName}! </h2>
-        <p>  You are on your way to finding your language partner and continuing your language learning journey. </p>
-        <p> An email containg instruction on your next steps has been sent to you!</p>
+        <div class="form-text">
+            <h2> Congradulation, ${partnerFullName}! </h2> </br>
+            <p>  You are on your way to finding your language partner and continuing your language learning journey. </p></br>
+            <p> An email containg instruction on your next steps has been sent to you!</p>
+
+        </div>
     `
 
     formContainer.innerHTML = newHtmlContent
