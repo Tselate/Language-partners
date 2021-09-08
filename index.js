3efa3a4d9da9b93313892c508733c8f20c89b333
Tselate <Tselatedawit@outlook.com>
2021-09-08 12:23:03 -0600

    Done

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
 let registartionForm = document.getElementById("partner-signup")
 let formContainer = document.querySelector(".form-container")
-let homePage = document.getElementById("home")
-let signUpPage = document.getElementById("signUp")
-
-
-//Current page indicator 
-
-
-
-
-homePage.addEventListener("click", function currentPage () {
-    homePage.classList.add("current-page")
-
-    
-})
-
-signUpPage.addEventListener("click", function currentPageS (e) {
-    e.preventDefault()
-    signUpPage.classList.add("current-page")
-})
-
 
 
 //Registration Form set up
@@ -34,7 +14,7 @@ registartionForm.addEventListener("submit", event => {
         <div class="form-text">
             <h2> Congradulation, ${partnerFullName}! </h2> </br>
             <p>  You are on your way to finding your language partner and continuing your language learning journey. </p></br>
-            <p> An email containg instruction on your next steps has been sent to you!</p>
+            <p> An email containg instructions on your next steps has been sent to you!</p>
 
         </div>
     `
